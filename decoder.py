9820ec67fde7f0ab4104850d6a64d00dbf8b244b
Samuel Wu <cernunnos1710@gmail.com>
2024-08-18 19:06:35 +0100

    initialize encoder decoder block classes

---
+++ decoder.py
@@ -0,0 +1,26 @@
+import multihead_attention as mult
+from torch import nn
+
+class DecoderBlock(nn.Module):
+    def __init__(self, input_dim: int, num_heads: int, ff_dim: int, dropout=0.0):
+        super().__init__()
+
+        # Self-attention layer
+        self.self_attention = mult.MultiHeadAttention(input_dim, input_dim, num_heads)
+
+        # Cross-attention layer
+        self.cross_attention = mult.MultiHeadAttention(input_dim, input_dim, num_heads)
+
+        # 2-Layer MLP
+        self.linear_MLP = nn.Sequential(
+            nn.Linear(input_dim, ff_dim),
+            nn.Dropout(dropout),
+            nn.ReLU(inplace=True),
+            nn.Linear(ff_dim, input_dim)
+        )
+
+        # Layers between main ones
+        self.norm1 = nn.LayerNorm(input_dim)
+        self.norm2 = nn.LayerNorm(input_dim)
+        self.norm3 = nn.LayerNorm(input_dim)
+        self.dropout = nn.Dropout(dropout)
