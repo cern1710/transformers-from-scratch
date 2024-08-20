72e88799c5b7e55ba50b6bbdc24c34f15240be08
Samuel Wu <cernunnos1710@gmail.com>
2024-08-20 14:23:40 +0100

    add an alternate scheduler (cosine decay) implementation for future use / comparisons

--- scheduler.py
+++ scheduler.py
@@ -18,3 +18,19 @@ class ExponentialDecayScheduler(optim.lr_scheduler._LRScheduler):
             decay = (epoch - self.warmup) / (self.max_iter - self.warmup)
             lr_factor = np.exp(-decay)
         return lr_factor
+
+class CosineDecayScheduler(optim.lr_scheduler._LRScheduler):
+    def __init__(self, optimizer, warmup, max_iter):
+        self.warmup = warmup
+        self.max_iter = max_iter
+        super().__init__(optimizer)
+
+    def get_lr(self):
+        lr_factor = self.get_lr_factor(epoch=self.last_epoch)
+        return [base_lr * lr_factor for base_lr in self.base_lrs]
+
+    def get_lr_factor(self, epoch):
+        lr_factor = 0.5 * (1 + np.cos(np.pi * epoch / self.max_iter))
+        if epoch <= self.warmup:
+            lr_factor *= epoch / self.warmup
+        return lr_factor
