665da6934c6c0b3e434cf8b6fd4c54433f229813
Samuel Wu <cernunnos1710@gmail.com>
2024-08-22 20:01:16 +0100

    Update scheduler with MultiStepLR for the ViT model

--- scheduler.py
+++ scheduler.py
@@ -34,3 +34,27 @@ class CosineWarmupScheduler(optim.lr_scheduler._LRScheduler):
         if epoch <= self.warmup:
             lr_factor *= epoch / self.warmup
         return lr_factor
+
+class MultiStepLRScheduler(optim.lr_scheduler._LRScheduler):
+    def __init__(self, optimizer, milestones,
+                 warmup = 0, gamma = 0.1, last_epoch= -1):
+        self.milestones = sorted(milestones)
+        self.gamma = gamma
+        self.warmup = warmup
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        lr_factor = self.get_lr_factor(epoch=self.last_epoch + 1)
+        return [base_lr * lr_factor for base_lr in self.base_lrs]
+
+    def get_lr_factor(self, epoch):
+        if epoch < self.warmup:  # Linear warmup
+            lr_factor = epoch / self.warmup
+        else:  # Multi-step decay
+            lr_factor = 1.0
+            for milestone in self.milestones:
+                if epoch >= milestone:
+                    lr_factor *= self.gamma
+                else:
+                    break
+        return lr_factor
