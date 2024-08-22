eb4014fec4d7e201973a5b22c791fc0fd5a645df
Samuel Wu <cernunnos1710@gmail.com>
2024-08-22 18:48:23 +0100

    Fully functional seq2seq training with transformers, with attention map plotting

--- train_seq2seq.py
+++ train_seq2seq.py
@@ -1,6 +1,5 @@
 import os
 import torch
-import numpy as np
 import matplotlib.pyplot as plt
 import dataset as ds
 import torch.utils.data as data
@@ -64,7 +63,7 @@ def train_seq2seq(**kwargs):
 
     criterion = nn.CrossEntropyLoss()
 
-    num_epochs = 10
+    num_epochs = 20
     best_val_accuracy = 0
     best_model_path = os.path.join("./path", "best_model.pth")
 
@@ -82,14 +81,47 @@ def train_seq2seq(**kwargs):
             best_val_accuracy = val_acc
             torch.save(model.state_dict(), best_model_path)
 
+        if best_val_accuracy == 1.0:
+            break
+
     model.load_state_dict(torch.load(best_model_path))
     test_loss, test_acc = eval(model, test_loader, criterion)
     print(f"Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}")
 
     return model, {'test_acc': test_acc, 'val_acc': best_val_accuracy}
 
+def plot_attention_maps(input_data, attn_maps, idx=0):
+    input_data = input_data[idx].detach().cpu().numpy()
+    attn_maps = [m[idx].detach().cpu().numpy() for m in attn_maps]
+
+    num_heads = attn_maps[0].shape[0]
+    seq_len = input_data.shape[0]
+    num_layers = len(attn_maps)
+    fig_size = 4 if num_heads == 1 else 3
+    fig, ax = plt.subplots(num_layers, num_heads,
+                           figsize=(num_heads*fig_size,
+                                    num_layers*fig_size))
+
+    # Ensure ax is a list of lists
+    if num_layers == 1:
+        ax = [ax]
+    if num_heads == 1:
+        ax = [[a] for a in ax]
+
+    for row in range(num_layers):
+        for column in range(num_heads):
+            ax[row][column].imshow(attn_maps[row][column],
+                                   origin='lower', vmin=0)
+            ax[row][column].set_xticks(list(range(seq_len)))
+            ax[row][column].set_xticklabels(input_data.tolist())
+            ax[row][column].set_yticks(list(range(seq_len)))
+            ax[row][column].set_yticklabels(input_data.tolist())
+            ax[row][column].set_title(f"Layer {row+1}, Head {column+1}")
+    fig.subplots_adjust(hspace=0.8)
+    plt.savefig('attention_maps.png')
+
 if __name__ == "__main__":
-    dataset = partial(ds.ReverseDataset, 10, 16)
+    dataset = partial(ds.ReverseDataset, 1000, 32)
     train_loader = data.DataLoader(dataset(50000), batch_size=128,
                                    shuffle=True, drop_last=True,
                                    pin_memory=True)
@@ -106,8 +138,15 @@ if __name__ == "__main__":
         num_heads=2,
         num_classes=train_loader.dataset.num_classes,
         num_layers=1,
-        dropout=0.0,
+        dropout=0.1,
         input_dropout=0.0,
         learning_rate=5e-4,
         warmup=50
     )
+    data_input, labels = next(iter(val_loader))
+    inp_data = F.one_hot(data_input, num_classes=reverse_model.
+                         num_classes).float()
+    inp_data = inp_data.to(device)
+    attention_maps = reverse_model.get_attention_maps(inp_data)
+
+    plot_attention_maps(data_input, attention_maps, idx=0)
